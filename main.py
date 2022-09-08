bb8cdd407a8d92d4954581ab7270fb47f5908cf0
ademilsonpires <89095980+ademilsonpires@users.noreply.github.com>
2022-09-08 19:50:26 -0300

    estrutura básica
    página contato e home, sem formatação

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/2')
-def hello_word():
-    return 'Helo Word'
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
