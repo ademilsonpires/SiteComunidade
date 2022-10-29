9950715e5f9c33d450377f6bbab0ed464a2eaaaf
ademilsonpires <89095980+ademilsonpires@users.noreply.github.com>
2022-10-28 21:58:35 -0300

    Criação de Models: classes usuário e post para as postagens na comunidade

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '9e57ca58a84f6cd3b02b2114fd90ef7c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+database = SQLAlchemy(app)
 
 @app.route('/')
 def home():
