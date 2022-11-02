c83f2991d2fb875fc93655d3551ce344d3aa3586
ademilsonpires <89095980+ademilsonpires@users.noreply.github.com>
2022-11-02 18:56:44 -0300

    Ajustes no banco de dados e testes de consulta e relacionamento

--- teste.py
+++ teste.py
@@ -13,6 +13,25 @@ from models import Usuario, Post
 #     database.session.add(usuario2)
 #     database.session.commit()
 
-with app.app_context():
-    meus_usuarios = Usuario.query.all()
-    print(meus_usuarios)
+# with app.app_context():
+#     meu_post = Post(id_usuario=1, titulo="Primeiro Post", corpo="Nosso primeiro post está de bo")
+#     meu_post2 = Post(id_usuario=2, titulo="Primeiro Post", corpo="Nosso primeiro post está de bo")
+#     database.session.add(meu_post)
+#     database.session.add(meu_post2)
+#     database.session.commit()
+
+# with app.app_context():
+#     meus_teste = Usuario.query.filter_by(id=2).first()
+#     print(meus_teste)
+#     print(meus_teste.username)
+
+# with app.app_context():
+#     meus_post = Post.query.filter_by(id_usuario=1).first()
+#    # print(meus_post)
+#     print(meus_post.titulo)
+#     print(meus_post.autor.username)
+#     print(meus_post.corpo)
+
+# with app.app_context():
+#     database.drop_all()
+#     database.create_all()
