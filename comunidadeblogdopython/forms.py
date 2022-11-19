eef4ec4ce4eecfd072dfdf07a56805d210ddd923
ademilsonpires <89095980+ademilsonpires@users.noreply.github.com>
2022-11-18 22:13:43 -0300

    edição de dados, validação de duplicidade e carregamento de dados no formulário de edição

--- comunidadeblogdopython/forms.py
+++ comunidadeblogdopython/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, length, Email, EqualTo, ValidationError
 from comunidadeblogdopython.models import Usuario
+from flask_login import current_user
 
 class FormCriarConta(FlaskForm):
     username = StringField('Nome de Usuário', validators=[DataRequired()])
@@ -31,4 +32,10 @@ class FormEditarPerfil(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     botao_submit_editar_perfil = SubmitField('Confirmar Edição')
 
+    def validate_email(self, email):
+        #verificar se usuário alterou o email
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError('Já existe um usuário com esse email, cadastre-se com outro email')
 
